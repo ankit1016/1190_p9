93c0261eca42940c2ccd42ad8a9b97324c1d05d1
ankit1016 <56350865+ankit1016@users.noreply.github.com>
2020-04-08 10:59:45 +0530

    Add files via upload

---
+++ 19bca1190 p9.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{ 
+cout<<"enter the 5 numbers";
+	int sum=0;
+	int  *myarray=NULL;
+	myarray=new int[5];
+	
+	for(int i=0;i<5;i++)
+	{
+		cin>>myarray[i];
+	}
+		cout<<"myarray value=";
+	
+		for(int i=0;i<5;i++)
+		{   
+		sum=sum+myarray[i];
+		}
+		
+		cout<<sum;
+	    delete[]myarray;
+}
